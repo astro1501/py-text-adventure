144f5ad37b0378551a8715bb8515bd5f8d222ae6
astro1501 <55759178+astro1501@users.noreply.github.com>
2020-11-15 23:28:27 -0500

    Add files via upload
    These are three uploaded files

--- adventure.py
+++ adventure.py
@@ -8,7 +8,7 @@ GAME = {
     }
 }
 
-def create_room(name, description, ends_game=False):
+def create_room(name, description, items, ends_game=False, cat=False):
     """
     Create a dictionary that represents a room in our game.
 
@@ -26,7 +26,11 @@ def create_room(name, description, ends_game=False):
         'description': description,
         'exits': [],
         'items': [],
+        'cat': cat
     }
+    
+    room['items'].append(items)
+    
     # Does this end the game?
     if ends_game:
         room['ends_game'] = ends_game
@@ -35,7 +39,7 @@ def create_room(name, description, ends_game=False):
     GAME[name] = room
     return room
 
-def create_exit(source, destination, description):
+def create_exit(source, destination, description, required_key=None, hidden=False):
     """
     Rooms are useless if you can't get to them! This function connects source to destination (in one direction only.)
 
@@ -55,8 +59,12 @@ def create_exit(source, destination, description):
     # Create the "exit":
     exit = {
         'destination': destination,
-        'description': description
+        'description': description,
+        'hidden' : hidden
     }
+    if required_key:
+        exit['required_key'] = required_key
+    
     source['exits'].append(exit)
     return exit
 
@@ -64,23 +72,51 @@ def create_exit(source, destination, description):
 # Let's imagine 4 places:
 # Note that earlier, in __metadata__ we said that we should start in classroom.
 ##
-classroom = create_room("classroom", "You're in a lecture hall, for some reason.")
-hallway = create_room("hallway", "This is a hallway with many locked doors.")
-staircase = create_room("staircase", "The staircase leads downward.")
-outside = create_room("outside", "You've escaped! It's cold out.", ends_game=True)
+classroom = create_room("classroom", "You're in a lecture hall, for some reason.", "Principle's ID")
+
+hallway = create_room("hallway", "This is a hallway with many locked doors.", "nothing")
+
+principle_office = create_room("Principles Office", "The drawers and shelves are overflowing with loose papers and confiscated objectes", "nothing")
+
+under_desk = create_room("Under Principle's desk", "A small red button is tucked in the back corner.", "nothing")
+
+janitors_closet = create_room("Janitor closet", "There are many wierd items scattered around.", "nothing")
+
+crawl_space = create_room("Crawl Space", "Hidden room behind utility shelf", "Janitor's key")
+
+staircase = create_room("staircase", "The staircase leads downward.", "nothing")
+
+cafeteria = create_room("Cafeteria", "Moldy food is strewn across the ground", "nothing")
+
+outside = create_room("outside", "You've escaped! It's cold out.", "nothing", ends_game=True)
 
 ##
 # Let's connect them together.
 # It's not a very fun adventure, but it's simple.
 ##
 create_exit(classroom, hallway, "A door leads into the hall.")
+
 # If you want doors to work in both directions; you have to do that yourself.
 create_exit(hallway, classroom, "Go back into the classroom.")
-# Note we can also refer to places by their names (same as their variables for my sanity!)
 create_exit('hallway', 'staircase', "A door with the words STAIRS is stuck open.")
-create_exit(staircase, hallway, "Nevermind; go back to the hallway.")
+create_exit(hallway, janitors_closet, "A door with a crooked janitors sign is locked")
+create_exit(hallway, principle_office, "Door seems bolted shut", required_key="Principle's ID")
 
-create_exit(staircase, outside, "A door at the bottom of the stairs has a red, glowing, EXIT sign.")
+create_exit(principle_office, under_desk, "If always wondered what the bottom of a desk looked like.")
+create_exit(principle_office, hallway, "This reminds me of trips to the office, lets leave.")
+
+create_exit(under_desk, principle_office, "Let's get out from under here, it smells like old sandwiches and dirty socks!")
+
+create_exit(janitors_closet, hallway, "Nothing in here, turn around")
+create_exit(janitors_closet, crawl_space, "There are scratch marks on the wall.", hidden=True)
+
+create_exit(crawl_space, janitors_closet, "Nothing else in here.")
+
+create_exit(cafeteria, staircase, "Is that a dead rat!! I gotta go!")
+
+create_exit(staircase, hallway, "Nevermind; go back to the hallway.")
+create_exit(staircase, cafeteria, "I smell something funky through here", required_key="Janitor's key")
+create_exit(staircase, outside, "A door at the bottom of the stairs has a red, glowing, EXIT sign.", required_key="Master key")
 # we don't go back from outside, because the game ends as soon as we get there.
 
 ##
--- play_game.py
+++ play_game.py
@@ -1,8 +1,10 @@
 import json
+import time
+import random
 
 def main():
     # TODO: allow them to choose from multiple JSON files?
-    with open('spooky_mansion.json') as fp:
+    with open('adventure.json') as fp:
         game = json.load(fp)
     print_instructions()
     print("You are about to play '{}'! Good luck!".format(game['__metadata__']['title']))
@@ -11,10 +13,13 @@ def main():
 
 
 def play(rooms):
+    start_time = time.time()
     # Where are we? Look in __metadata__ for the room we should start in first.
     current_place = rooms['__metadata__']['start']
     # The things the player has collected.
-    stuff = ['Cell Phone; no signal or battery...']
+    stuff = ['Cell Phone; no signal or battery...', '3 quarters', '2 dimes']
+
+    # allows us to see the whole dict: print(rooms)
 
     while True:
         # Figure out what room we're in -- current_place is a name.
@@ -24,10 +29,26 @@ def play(rooms):
 
         # TODO: print any available items in the room...
         # e.g., There is a Mansion Key.
+        if current_place != "outside":
+            temp = here['items']
+            print("There is {} in this room".format(temp[0]))
 
         # Is this a game-over?
         if here.get("ends_game", False):
             break
+
+        # Clears all locations of a cat then chooses random location to place a cat
+        for all_rooms in rooms:
+            if all_rooms != '__metadata__':
+                rooms[all_rooms]['cat'] = False
+        room_names = [] 
+        for key in rooms.keys(): 
+            room_names.append(key)   
+        del room_names[0]
+        rand_place = random.choice(room_names)
+        rooms[rand_place]['cat'] = True
+        if here.get("cat", True):
+            print("A black cat has wandered into this room.")
 
         # Allow the user to choose an exit:
         usable_exits = find_usable_exits(here, stuff)
@@ -44,21 +65,65 @@ def play(rooms):
             break
 
         # TODO: if they type "stuff", print any items they have (check the stuff list!)
-        # TODO: if they type "take", grab any items in the room.
-        # TODO: if they type "search", or "find", look through any exits in the room that might be hidden, and make them not hidden anymore!
+        if action == 'stuff':
+            print(stuff)
+            continue
         
+        # Create a "push button" command, this is my big switch, it makes items appear
+        if action == "push button":
+            change_item = rooms["Cafeteria"]
+            change_item['items'][0] = "Master key"
+            print("You here something clatter on the floor, downstairs.")
+            continue
+        
+        # If drop is typed, I am being lazy and only allowing one item to be
+        # assigned to a room at once and you can only drop the last item picked up.
+        if action == 'drop':
+            here['items'][0] = stuff[len(stuff)-1]
+            del stuff[len(stuff)-1]
+            continue
+        
+        # TODO: if they type "take", grab any items in the room.
+        if action == 'take':
+            temp = here['items']
+            if temp[0] != 'nothing':
+                stuff.append(temp[0])
+                here['items'][0] = "nothing"
+                continue
+            print("You can't take 'nothing', silly!")
+            continue
+            
+        # TODO: if they type "search", or "find", look through any exits in the room that might be hidden, and make them not hidden anymore!
+        if action == "search" or action == 'find':
+            for exit in here['exits']:
+                if exit.get("hidden", True):
+                    exit["hidden"] = False
+            continue    
+        
+        if action == 'help':
+            print_instructions()
+            continue
         # Try to turn their action into an exit, by number.
         try:
             num = int(action) - 1
             selected = usable_exits[num]
+            if "required_key" in selected:
+                if selected["required_key"] in stuff:
+                    current_place = selected['destination']
+                else:
+                    print("This room is locked. Search for the key.")
+                    continue
             current_place = selected['destination']
             print("...")
         except:
             print("I don't understand '{}'...".format(action))
-        
+    
+    end_time = time.time()
+    elapsed = end_time - start_time
     print("")
     print("")
     print("=== GAME OVER ===")
+    print("Took you {} seconds to complete".format(int(elapsed)))
 
 def find_usable_exits(room, stuff):
     """
@@ -72,10 +137,6 @@ def find_usable_exits(room, stuff):
     for exit in room['exits']:
         if exit.get("hidden", False):
             continue
-        if "required_key" in exit:
-            if exit["required_key"] in stuff:
-                usable.append(exit)
-            continue
         usable.append(exit)
     return usable
 
@@ -86,6 +147,7 @@ def print_instructions():
     print(" - Type 'take' to pick up an item.")
     print(" - Type 'quit' to exit the game.")
     print(" - Type 'search' to take a deeper look at a room.")
+    print(" - 'push button' is a command that might come in handy.")
     print("=== Instructions ===")
     print("")
 
